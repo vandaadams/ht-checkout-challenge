5e2654d245f7c979c5a1eed5bda5f582ba7134c0
Vanda Adams <vandamadams@gmail.com>
2020-03-29 12:38:41 +0100

    added js to get user input from form

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+// 1. populate radio elements of form from api
 const productNames = document.querySelectorAll("#product-name");
 const productPrices = document.querySelectorAll("#product-price");
-// console.log(prices);
 
 fetch("http://my-json-server.typicode.com/vandaadams/ht-checkout-challenge/db")
   .then(response => response.json())
@@ -12,14 +12,24 @@ fetch("http://my-json-server.typicode.com/vandaadams/ht-checkout-challenge/db")
     productNames.forEach((product) => {
       product.insertAdjacentText("afterend", `${results[counter].name}`);
       counter += 1;
-      // console.log(product);
     });
 
     counter = 0
     productPrices.forEach((product) => {
       product.textContent = `${results[counter].price}`;
       counter += 1;
-      // console.log(product);
     });
 
   });
+
+// 2. send post request to api when submit button is clicked
+const form = document.getElementById("form");
+
+form.addEventListener('submit', ((element) => {
+  element.preventDefault();
+  firstName = document.getElementById("fname").value;
+  lastName = document.getElementById("lname").value;
+  email = document.getElementById("email").value;
+  // console.log(firstName, lastName, email)
+});
+);
